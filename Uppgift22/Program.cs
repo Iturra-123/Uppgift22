9d6222567d916231d26fef6edfd736176a924750
leit07001 <leit07001@E-PW05C37S>
2024-10-10 13:01:30 +0200

    Add project files.

---
+++ Uppgift22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace uppgift22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elin hoppade 2 Meter);");
+            double Elinhopp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Alma hoppade 1 Meter");
+            double almahopp = Convert.ToDouble(Console.ReadLine());
+
+            double skillnad = Elinhopp - almahopp;
+
+            Console.WriteLine("Elin hoppade 1 meter längre än Alma");
+        }
+    }
+}
